360a99a75080a8c4b46053df752266712f3f8654
Szacsuri Norbert <szacskesz@gmail.com>
2019-04-20 16:14:01 +0200

    removed metadata from log

--- src/service/OrderService.js
+++ src/service/OrderService.js
@@ -3,7 +3,6 @@ function OrderService() {
     logger = winston.createLogger({
         level: 'info',
         format: winston.format.json(),
-        defaultMeta: { service: 'order-service' },
         transports: [
             new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
             new winston.transports.File({ filename: 'logs/combined.log' })
--- src/service/ShutterDataService.js
+++ src/service/ShutterDataService.js
@@ -3,7 +3,6 @@ function ShutterDataService() {
     logger = winston.createLogger({
         level: 'info',
         format: winston.format.json(),
-        defaultMeta: { service: 'shutter-data-service' },
         transports: [
             new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
             new winston.transports.File({ filename: 'logs/combined.log' })
