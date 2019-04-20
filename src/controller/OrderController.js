feb5206e9d057c1c4b619d3dd0113a1a22a5f027
Szacsuri Norbert <szacskesz@gmail.com>
2019-04-20 14:46:40 +0200

    Controller send fix + order fixes

--- src/controller/OrderController.js
+++ src/controller/OrderController.js
@@ -1,74 +1,75 @@
 const routes = require("express").Router();
 
+var InvoiceClass = require("../models/Invoice");
+var OrderClass = require("../models/Order");
+
 var OrderService = require("../service/OrderService");
 const orderService = new OrderService();
-var InvoiceClass = require("../models/Invoice");
+
+const checkForHexRegExp = new RegExp("^[0-9a-fA-F]{24}$");
 
 routes.get("/getAllOrders", (req, resp) => {
     orderService.getAllOrders((orders) => {
-        resp.status(200).send(orders);
+        resp.status(200).send({"orders": orders});
     }, (error) => {
-        resp.status(400).send(error);
+        resp.status(400).send({"error": error});
     });
 })
 
 routes.post("/getAllOrdersByUsername", (req, resp) => {
     if(req.body["username"] == undefined) {
-        resp.status(400).send("username must be defined");
+        resp.status(400).send({"error": "username must be defined"});
         return;
     }
 
     orderService.getAllOrdersByUsername(req.body["username"], (orders) => {
-        resp.status(200).send(orders);
+        resp.status(200).send({"orders": orders});
     }, (error) => {
-        resp.status(400).send(error);
+        resp.status(400).send({"error": error});
     });
 })
 
 routes.post("/createOrder", (req, resp) => {
+    let order;
+    try {
+        order = new OrderClass.OrderFromJson(req.body["order"]);
+    } catch (error) {
+        resp.status(400).send({"error": error});
+        return;
+    }
 
-    //TODO CHECKS
-
-    //TODO return id
-
-    //TODO create data
-    let order = {
-        
-    };
-
-    orderService.createOrder(order, () => {
-        resp.status(200).send();
+    orderService.createOrder(order, (createdId) => {
+        resp.status(200).send({"createdId": createdId});
     }, (error) => {
-        resp.status(400).send(error);
+        resp.status(400).send({"error": error});
     });
 })
 
 routes.post("/finishShutter", (req, resp) => {
     if(req.body["orderId"] == undefined) {
-        resp.status(400).send("orderId must be defined");
+        resp.status(400).send({"error": "orderId must be defined"});
         return;
     }
     if(req.body["shutterId"] == undefined) {
-        resp.status(400).send("shutterId must be defined");
+        resp.status(400).send({"error": "shutterId must be defined"});
+        return;
+    }
+
+    if(!checkForHexRegExp.test(req.body["orderId"])) {
+        resp.status(400).send({"error": "orderId must be 24 hex string"});
         return;
     }
 
     orderService.finishShutter(req.body["orderId"], req.body["shutterId"], () => {
         resp.status(200).send();
     }, (error) => {
-        resp.status(400).send(error);
+        resp.status(400).send({"error": error});
     });
 })
 
-routes.post("/createInvoiceForOrder/:orderId", (req, resp) => {
-    if(req.params.orderId == undefined) {
-        resp.status(400).send("orderId must be defined in the path");
-        return;
-    }
-
-    let orderId = Number(req.params.orderId)
-    if(isNaN(orderId)) {
-        resp.status(400).send("orderId must be a number");
+routes.post("/createInvoiceForOrder", (req, resp) => {
+    if(req.body["orderId"] == undefined) {
+        resp.status(400).send({"error": "orderId must be defined"});
         return;
     }
 
@@ -76,14 +77,19 @@ routes.post("/createInvoiceForOrder/:orderId", (req, resp) => {
     try {
         invoice = new InvoiceClass.InvoiceFromJson(req.body["invoice"]);
     } catch (error) {
-        resp.status(400).send("" + error);
+        resp.status(400).send({"error": error});
         return;
     }
 
-    orderService.createInvoiceForOrder(orderId, invoice, () => {
+    if(!checkForHexRegExp.test(req.body["orderId"])) {
+        resp.status(400).send({"error": "orderId must be 24 hex string"});
+        return;
+    }
+
+    orderService.createInvoiceForOrder(req.body["orderId"], invoice, () => {
         resp.status(200).send();
     }, (error) => {
-        resp.status(400).send(error);
+        resp.status(500).send({"error": error});
     });
 })
 
--- src/controller/ShutterDataController.js
+++ src/controller/ShutterDataController.js
@@ -5,25 +5,25 @@ const shutterDataService = new ShutterDataService();
 
 routes.get("/getAllShutterMaterials", (req, resp) => {
     shutterDataService.getAllShutterMaterials((materials) => {
-        resp.status(200).send(materials);
+        resp.status(200).send({"materials": materials});
     }, (error) => {
-        resp.status(400).send(error);
+        resp.status(400).send({"error": error});
     });
 })
 
 routes.get("/getAllShutterTypes", (req, resp) => {
     shutterDataService.getAllShutterTypes((types) => {
-        resp.status(200).send(types);
+        resp.status(200).send({"types": types});
     }, (error) => {
-        resp.status(400).send(error);
+        resp.status(400).send({"error": error});
     });
 })
 
 routes.get("/getAllShutterColors", (req, resp) => {
     shutterDataService.getAllShutterColors((colors) => {
-        resp.status(200).send(colors);
+        resp.status(200).send({"colors": colors});
     }, (error) => {
-        resp.status(400).send(error);
+        resp.status(400).send({"error": error});
     });
 })
 
