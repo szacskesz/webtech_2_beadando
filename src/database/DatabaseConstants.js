94d953366894fdce46add3133ad160b0a850f5f0
Szacsuri Norbert <szacskesz@gmail.com>
2019-05-07 16:12:30 +0200

    server url fix

--- src/database/DatabaseConstants.js
+++ src/database/DatabaseConstants.js
@@ -1,6 +1,6 @@
 module.exports = {
     dbName : 'webtech_2_assignment',
-    url: 'mongodb://localhost:27017"',
+    url: '172.21.0.10:27017"',
     collections : {
         shutterColors : 'shutter_colors',
         shutterTypes : 'shutter_types',
